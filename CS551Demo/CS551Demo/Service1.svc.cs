ab7082b19f5063260772c37b6b4341d2be3a140f
vp988 <vp988@mail.umkc.edu>
2015-02-27 17:05:49 -0600

    Demo Visual Studio
    VS

---
+++ CS551Demo/CS551Demo/Service1.svc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CS551Demo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+
+
+    [WebInvoke(Method = "GET",ResponseFormat = WebMessageFormat.Json,UriTemplate = "data/{id}")]
+
+        public String GetDetails(string id)
+        {
+           return string.Format("you entered:{0}",id);    
+        // lookup person with the requested id 
+           /* return new Person()
+            {
+                id = Convert.ToInt32(id),
+                
+                
+                name = "Student"+id
+                
+            };*/
+        }
+
+
+
+    }
+
+    
+        public class Person
+        {
+
+            public int id { get; set; }
+
+            public string name { get; set; }
+
+        }
+}
---
+++ CS551Demo/CS551Demo/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CS551Demo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <script type='text/javascript' src='http://code.jquery.com/jquery-1.8.3.js'></script>
+    <script type = "text/javascript">
+        function wscall() {
+            var name = document.getElementById("uname");
+           // alert(name.value);
+            var data1 = name.value;
+            $.get("http://localhost:60574/Service1.svc/data/"+data1, function (data) {
+                    
+                $("#formdiv").html(data);
+            });
+
+           /* window.location.href="http://localhost:60574/Service1.svc/data/"+data;*/
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="formdiv">
+    <label for="username">UserName:</label>
+      <input type="text" id="uname" size="30" />
+        <br />
+        <label for="Pwd">Password:</label>
+      <input type="password" id="pwd" size="30" />
+      <br />
+     <input type ="button"  value="Login" onclick="wscall()"/>
+    </div>
+    </form>
+</body>
+</html>
